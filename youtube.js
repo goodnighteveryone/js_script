8f002a9edc2384ab4b2b38f8e6f3bb7c33b93d50
翟树杰 <hello_worldzsj@qq.com>
2023-10-13 14:07:35 +0800

    油管去会员弹窗脚本1.1

--- youtube.js
+++ youtube.js
@@ -10,13 +10,26 @@
 // ==/UserScript==
 
 (function () {
-  var dialog = document.getElementsByClassName("ytd-popup-container")[0];
-  console.log(dialog, "dialog");
-  if (dialog) {
-    var cancel = dialog.getElementsByClassName(
-      "yt-spec-touch-feedback-shape__fill"
-    )[0];
-    console.log(cancel, "cancel");
-    cancel && cancel.click();
+  let observer = new MutationObserver(handleUrlChange);
+  let config = { childList: true, subtree: false };
+  observer.observe(document.getElementById("movie_player"), config);
+  var curUrl = window.location.href;
+  function handleUrlChange() {
+    console.log("111", curUrl);
+    if (curUrl !== window.location.href) {
+      console.log("222");
+      setTimeout(() => {
+        let dialog = document.getElementsByClassName("ytd-popup-container")[0];
+        console.log(dialog, "dialog");
+        curUrl = window.location.href;
+        if (dialog) {
+          let cancel = dialog.getElementsByClassName(
+            "yt-spec-touch-feedback-shape__fill"
+          )[0];
+          console.log(cancel, "cancel");
+          cancel && cancel.click();
+        }
+      }, 500);
+    }
   }
 })();
